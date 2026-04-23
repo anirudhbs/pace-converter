ea805c3ef4e8fa6a2d77f6ac51228de52f6744e3
Anirudh Bangalore Srinivas <25001871+anirudhbs@users.noreply.github.com>
2026-04-23 14:32:16 +0530

    Fix deployment issue in vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
+  base: "./",
   root: "src",
   build: {
     outDir: "../dist",
